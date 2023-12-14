caa7e426ed5a3250f3a25c11990ab6fa2787976e
= <=>
2023-12-14 15:54:16 +0300

    contact us page

--- website/components/footer.php
+++ website/components/footer.php
@@ -1,3 +1,39 @@
 <div class="footer">
-    <!-- <img src="./assets/images/background_1.png" alt=""> -->
+    <div class="footer-bg">
+        <div id="footer-container" class="flex-row flex-wrap">
+            <div class="container-50">
+                <h3 class="mb-10">Stay in Touch</h3>
+                <a href="tel:+24450069" class="footer-link">
+                    <div class="icon">
+                        <img src="./assets/icons/icon_phone.png" alt="📞">
+                    </div>
+                    <div class="text">
+                        +24450068
+                    </div>
+                </a>
+                <a href="mailto:sales@rwadseafood.com" class="footer-link">
+                    <div class="icon">
+                        <img src="./assets/icons/icon_mail.png" alt="✉️">
+                    </div>
+                    <div class="text">
+                        sales@rwadseafood.com
+                    </div>
+                </a>
+                <a href="#" class="footer-link">
+                    <div class="icon">
+                        <img src="./assets/icons/icon_location.png" alt="✉️">
+                    </div>
+                    <div class="text">
+                        Mabella Indstial Area, Sultanate of Oman
+                    </div>
+                </a>
+                <div class="mt-20" id="footer-fish">
+                    <img src="./assets/icons/fish_school.png" alt="">
+                </div>
+            </div>
+            <div class="container-50" id="footer-right">
+                <img src="./assets/icons/man_fishing.png" alt="">
+            </div>
+        </div>
+    </div>
 </div>
--- website/contacts.php
+++ website/contacts.php
@@ -3,14 +3,83 @@
 
 <head>
     <?php include_once("components/header-imports.php") ?>
-    <title>RWAD :: Empty</title>
+    <title>RWAD :: Contacts</title>
 </head>
 
 <body id="">
     <?php include_once("components/top-bar.php") ?>
     <div class="main-container">
-        <div class="spacer"></div>
-        EMPTY
+        <div id="contacts-cover">
+            <h1 class="text-align-center">Contact Us</h1>
+        </div>
+        <div class="flex-row app-padding">
+            <!-- form -->
+            <div class="container-50 p-50">
+                <h4 class="mb-20">Need to order our products, ask a question or simply say hi? Don't hesitate to message us!</h4>
+                <div class="app-form flex-row mb-50">
+                    <div class="container-100">
+                        <div id="contact-img">
+                            <img src="./assets/icons/contact_us.png" alt="">
+                        </div>
+                    </div>
+                    <div class="container-50 p-5">
+                        <div class="form-input">
+                            <label for="first-name">First Name *</label>
+                            <input type="text" required name="first-name" id="first-name">
+                        </div>
+                    </div>
+                    <div class="container-50 p-5">
+                        <div class="form-input">
+                            <label for="last-name">Last Name *</label>
+                            <input type="text" required name="last-name" id="last-name">
+                        </div>
+                    </div>
+                    <div class="container-50 p-5">
+                        <div class="form-input">
+                            <label for="phone">Phone Number (optional)</label>
+                            <input type="phone" name="phone" id="phone">
+                        </div>
+                    </div>
+                    <div class="container-50 p-5">
+                        <div class="form-input">
+                            <label for="email">Email *</label>
+                            <input type="email" required name="email" id="email">
+                        </div>
+                    </div>
+                    <div class="container-50 p-5">
+                        <div class="form-input">
+                            <label for="address">Address (optional)</label>
+                            <input type="address" name="address" id="address">
+                        </div>
+                    </div>
+                    <div class="container-50 p-5">
+                        <div class="form-input">
+                            <label for="enquiry-type">Enquiry Type</label>
+                            <input type="enquiry-type" required name="enquiry-type" id="enquiry-type">
+                        </div>
+                    </div>
+                    <div class="container-100 p-5">
+                        <div class="form-input">
+                            <label for="enquiry-type">Message</label>
+                            <textarea name="message" id="message"></textarea>
+                        </div>
+                    </div>
+                    <div class="container-100  flex-row justify-content-center ">
+                        <button id="button-submit">
+                            SUBMIT
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <!-- form -->
+
+            <!-- map -->
+            <div class="container-50 p-50">
+                <h3>Where to find us</h3>
+                <iframe class="mb-50 mt-10" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d36688.18291615615!2d58.07200756596166!3d23.646875033878608!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e8de41de1ea537d%3A0xd4bfc9d36553eb51!2sMa&#39;abilah%20Industrial%20Area%2C%20Seeb%2C%20Oman!5e0!3m2!1sen!2stz!4v1702548117720!5m2!1sen!2stz" width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <!-- map -->
+        </div>
     </div>
     <?php include_once("components/footer.php") ?>
     <?php include_once("components/script-imports.php") ?>
