38816045aeabfd6cc08665d9b5fff6bc05f19201
= <=>
2024-05-20 12:39:28 +0300

    minor

--- website/api/sendProductCatalog.php
+++ website/api/sendProductCatalog.php
@@ -12,24 +12,19 @@ if (
     $year = date("Y");
 
     if ($token === "HadksljlHKASdj123SDLAJFD!133") {
-        $headers = "From: RWAD Seafood Website<sales@rwadseafood.com>\r\n";
-        $headers .= "Reply-To: sales@rwadseafood.com\r\n";
-        $headers .= "Return-Path: sales@rwadseafood.com\r\n";
+        $headers = "From: RWAD Seafood Website<admin@rwadseafood.com>\r\n";
+        $headers .= "Reply-To: admin@rwadseafood.com\r\n";
+        $headers .= "Return-Path: admin@rwadseafood.com\r\n";
+        $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
         $subject = "RWAD Seafood Product Catalog";
 
         $email_message = "Hello, thank you for requesting product catalog from RWAD seafood.\n\n
-        Click on the link below to download and save the PDF file.\n\n
-        <html>
-            <body>
-                <center>
-                    <a  style='text-decoration:none' href='https://rwadseasood.com/assets/pdf/rwad-product-catalog.pdf' >
-                        <button style='height:80px; width:100px; padding:10px; background:dodgerblue; color:white; border-radius:10px' >
-                            Download
-                        </button>
-                    </a>
-                </center>
-            </body>
-        </html>
+        Click on the link below to download and save the PDF file.\n\r\n\r
+        <div style='padding:50px 0;'>
+            <a href='https://rwadseafood.com/assets/pdf/rwad-product-catalog.pdf' style='padding: 8px 12px; background:dodgerblue; color:white; text-decoration:none;'>
+                DOWNLOAD PDF
+            </a>
+        </div>
         \r\n\r\n";
         $email_message .= "Copyright RWAD Seafood $year \r\n";
 
@@ -38,7 +33,7 @@ if (
             mail("athmanmbwana619@gmail.com", $subject, "Sent product catalog to $email", $headers);
             mail("sales@rwadseafood.com", $subject, "Sent product catalog to $email", $headers);
             $response->message = "success";
-            $response->data = "Thank requesting product catalog. Please check your email for the download link";
+            $response->data = "Thanks for requesting our product catalog. Please check your email for the download link";
         } else {
             $response->message = "error";
             $response->data = "Error, mail service unavailable.";
